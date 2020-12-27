3bd5d02a8838aee19562e188b9b5b9519423a100
Rodrigo Mayer <mayerrodrigo98@gmail.com>
2020-12-27 00:46:48 -0300

    1006

---
+++ python/1006.py
@@ -0,0 +1,12 @@
+A = float(input())
+B = float(input())
+C = float(input())
+
+weightA = 2.0
+weightB = 3.0
+weightC = 5.0
+totalWeight = weightA + weightB + weightC
+
+avg = (A * weightA + B * weightB + C * weightC)/totalWeight
+
+print("MEDIA = %.1f" % avg)
